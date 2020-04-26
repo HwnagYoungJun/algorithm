8947b6a87ae72708888a057ab8a068630c98285a
HwangYoungJun <dudghkd4512@gmail.com>
2020-04-26 23:57:08 +0900

    200426

--- 0426/1987_알파벳_백준.py
+++ 0426/1987_알파벳_백준.py
@@ -1,36 +1,32 @@
 import sys
 sys.stdin = open('1987.txt')
-input = sys.stdin.readline
-import collections
 
-dr = [0, 0, -1, 1]
-dc = [-1, 1, 0, 0]
+# 좌, 하, 우, 상
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-def bfs():
-    global cnt
-    deq = collections.deque()
-    deq.append((0, 0, {board[0][0]})) # r,c 와 count를 담자!
-    # 방문은 루트마다 다르므로 다르게 관리한다
 
-    while deq:
-        row, col, foot_step = deq.popleft()
+def BFS(x, y):
+    global answer
+    q = set([(x, y, board[x][y])])
 
-        for w in range(4):
-            nr = row + dr[w]
-            nc = col + dc[w]
+    while q:
+        x, y, ans = q.pop()
 
-            if 0 <= nr < R and 0 <= nc < C:
-                if board[nr][nc] not in foot_step:
-                    deq.append((nr, nc, foot_step | {board[nr][nc]}))
-                    cnt = max(len(foot_step) + 1, cnt)
-                    
+        # 좌우상하 갈 수 있는지 살펴본다
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-# 첫 느낌 : SWEA의 수지의 수지맞는 여행이랑 비슷하다는 느낌을 받음
+            # index 벗어나지 않는지 체크하고, 새로운 칸이 중복되는 알파벳인지 체크한다
+            if ((0 <= nx < R) and (0 <= ny < C)) and (board[nx][ny] not in ans):
+                q.add((nx,ny,ans + board[nx][ny]))
+                answer = max(answer, len(ans)+1)
 
-R, C = map(int, input().split())
-board = [list(input()) for _ in range(R)]
-cnt = 1
-bfs()
-print(cnt)
 
-# 끝 느낌 : dfs로 풀껄
+R, C = map(int, sys.stdin.readline().split())
+board = [list(sys.stdin.readline().strip()) for _ in range(R)]
+
+answer = 1
+BFS(0, 0)
+print(answer)
