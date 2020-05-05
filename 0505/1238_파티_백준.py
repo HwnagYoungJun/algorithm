7d36be99ddbb98ee8819ff4bf9254ae4e545af3e
HwangYoungJun <dudghkd4512@gmail.com>
2020-05-05 23:18:15 +0900

    200505

--- 0505/1238_파티_백준.py
+++ 0505/1238_파티_백준.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open('1238.txt')
 import collections
 
-def dijkstra():
+def dijkstra(g):
     
     dijk = [float('inf') for _ in range(N + 1)]
     visit = [0 for _ in range(N + 1)]
@@ -12,7 +12,7 @@ def dijkstra():
     while True:
 
         # 다익스트라의 첫번쨰는 최신화시켜주기!
-        for nc, time in connect[index]:
+        for nc, time in g[index]:
             if visit[nc] == 0:
                 via = dijk[index] + time
                 if dijk[nc] > via:
@@ -31,38 +31,21 @@ def dijkstra():
 
     return dijk
 
-def dfs(start, i, dis):
-    global temp
-    visit[i] = 1
-    if dis >= temp[start]:
-        return
-    if i == X:
-        if temp[start] > dis:
-            temp[start] = dis
-        return
-    else:
-
-        for ni,time in connect[i]:
-            if visit[ni] == 0:
-                visit[ni] = 1
-                dfs(start, ni, dis + time)
-                visit[ni] = 0
-            
 N, M, X = map(int, input().split())
 
 connect = [[] for _ in range(N + 1)]
-
+rev_connect = [[] for _ in range(N + 1)]
 for _ in range(M):
     S, E, T = map(int, input().split())
-
     connect[S].append((E, T))
+    rev_connect[E].append((S, T))
 
-귀가 = dijkstra()
-temp = [float('inf') for _ in range(N + 1)]
-for index in range(1, N + 1):
-    if index != X:
-        visit = [0 for _ in range(N + 1)]
-        dfs(index, index, 0)
-        귀가[index] += temp[index]
-귀가[0] = float('-inf')
-print(max(귀가))
+귀가 = dijkstra(connect)
+역귀가 = dijkstra(rev_connect)
+result = float('-inf')
+
+for i in range(1, N + 1):
+    if result < 귀가[i] + 역귀가[i]:
+        result = 귀가[i] + 역귀가[i]
+
+print(result)
