c9138c9eeeb1e6e62993b8d28403ae6ba21f968a
HwangYoungJun <dudghkd4512@gmail.com>
2020-05-10 01:57:59 +0900

    200510

--- 0510/9466_텀프로젝트_백준.py
+++ 0510/9466_텀프로젝트_백준.py
@@ -1,24 +1,25 @@
 import sys
 sys.stdin = open('9466.txt')
-sys.setrecursionlimit(150000)
+sys.setrecursionlimit(111111)
 input = sys.stdin.readline
 
 # 배운점 : 재귀깊이를 늘리면 메모리초과가 날 수 있다.
 #         DFS를 쓸꺼라면 재귀 제한은 미리 해제해두자.
-def dfs(parent, s):
-    global already_team
-    global not_team
+def dfs(s):
     global team
+    global already_team
+    global no_friend
 
-    if parent == s and s in team:
-        for i in team:
-            already_team[i] = 1
-        not_team -= len(team)
-        return
+    already_team[s] = 1
+    team.append(s)
 
-    if classroom[s] not in team and already_team[classroom[s]] == 0:
-        team.add(classroom[s])
-        dfs(parent, classroom[s])
+    next_student = classroom[s]
+    if already_team[next_student] == 1:
+        if next_student in team:
+            no_friend -= len(team[team.index(next_student):])
+            return
+    else:
+        dfs(next_student)
 
 T = int(input())
 
@@ -27,10 +28,11 @@ for test_case in range(1, T + 1):
     classroom = ['X'] + list(map(int, input().split()))
     
     already_team = [0 for _ in range(N + 1)]
-    not_team = N
+    already_team[0] = float('inf')
+
+    no_friend = N
     for student in range(1, N + 1):
         if already_team[student] == 0:
-            team = set()
-            dfs(student, student)
-        
-    print(not_team)
+            team = list()
+            dfs(student)
+    print(no_friend)
