b23384988eb69dbede5b0df4f9efaae6e6485e25
HwangYoungJun <dudghkd4512@gmail.com>
2020-10-02 23:29:28 +0900

    201002

---
+++ 10월/1002/5567_결혼식_백준.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1002
+{
+    class _5567_결혼식_백준
+    {
+        static int n, m;
+        static Dictionary<int, List<int>> relation;
+        static void BFS()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            // input
+            n = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            relation = new Dictionary<int, List<int>>();
+            for (int i = 0; i < 5; i++)
+            {
+                var temp = Console.ReadLine().Split();
+                var friend1 = int.Parse(temp[0]);
+                var friend2 = int.Parse(temp[1]);
+                relation[friend1].Add(friend2);
+                relation[friend2].Add(friend1);
+            }
+
+        }
+    }
+}
